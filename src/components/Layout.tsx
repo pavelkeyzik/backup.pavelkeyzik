dd506e65f3b19d36ab9a450c4d1e7be22420b77e
Pavel Keyzik <pavel.keyzik@gmail.com>
2021-02-28 02:00:50 +0300

    Add isActive for links in Header menu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import { useRouter } from 'next/dist/client/router';
 import { Header } from '../components/Header';
 import { ToggleThemButton } from '../components/ToggleThemeButton';
 import { Footer } from './Footer';
@@ -11,6 +13,8 @@ type LayoutProps = {
 };
 
 function Layout(props: LayoutProps) {
+  const route = useRouter();
+
   return (
     <div
       style={{
@@ -28,9 +32,18 @@ function Layout(props: LayoutProps) {
         </Header.Item>
         <Header.Item>
           <Nav>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact</Link>
+            <ActiveLink isActive={route.pathname === '/'} href="/">
+              Home
+            </ActiveLink>
+            <ActiveLink isActive={route.pathname === '/about'} href="/about">
+              About
+            </ActiveLink>
+            <ActiveLink
+              isActive={route.pathname === '/contact'}
+              href="/contact"
+            >
+              Contact
+            </ActiveLink>
           </Nav>
           <ToggleThemButton />
         </Header.Item>
@@ -40,6 +53,38 @@ function Layout(props: LayoutProps) {
     </div>
   );
 }
+
+function ActiveLink(
+  props: React.PropsWithChildren<LinkProps & { isActive?: boolean }>
+) {
+  const { isActive, ...rest } = props;
+
+  return (
+    <Root isActive={isActive}>
+      <Link {...rest} />
+    </Root>
+  );
+}
+
+const Root = styled.span<{ isActive?: boolean }>`
+  position: relative;
+
+  ${(props) =>
+    props.isActive &&
+    css`
+      ::after {
+        content: '';
+        position: absolute;
+        display: flex;
+        top: 26px;
+        left: calc(50% - 8px);
+        width: 16px;
+        height: 4px;
+        border-radius: 4px;
+        background: ${props.theme.color.primary};
+      }
+    `}
+`;
 
 const Nav = styled.nav`
   & > * {
