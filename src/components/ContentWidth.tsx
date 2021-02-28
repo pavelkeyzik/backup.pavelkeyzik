7e1298554aeb303612c6223ae8b58a9f2d326fb9
Pavel Keyzik <pavel.keyzik@gmail.com>
2021-02-28 14:49:09 +0300

    Update horizontal paddings for ContentWidth component on mobile

--- src/components/ContentWidth.tsx
+++ src/components/ContentWidth.tsx
@@ -3,8 +3,12 @@ import styled from '@emotion/styled';
 const ContentWidth = styled.div`
   width: 100%;
   max-width: 1180px;
-  padding: 0 40px;
+  padding: 0 20px;
   margin: 0 auto;
+
+  @media screen and (min-width: 600px) {
+    padding: 0 40px;
+  }
 `;
 
 export { ContentWidth };
