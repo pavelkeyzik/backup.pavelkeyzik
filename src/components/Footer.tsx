8ed7a42813f48f8ccf97f6550890f1406dd69b31
Pavel Keyzik <pavel.keyzik@gmail.com>
2021-02-28 23:40:13 +0300

    Add GitHub and Twitter links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 import { ContentWidth } from './ContentWidth';
+import { GitHubIcon } from './icons/GitHubIcon';
+import { TwitterIcon } from './icons/TwitterIcon';
 
 type FooterProps = {
   style?: React.CSSProperties;
@@ -9,7 +11,18 @@ function Footer(props: FooterProps) {
   return (
     <Root style={props.style}>
       <ContentWidth>
-        Created by Pavel Keyzik with love at deep night...
+        <FooterContent>
+          <Nav>
+            <SocialLink href="https://twitter.com/pavelkeyzik" target="_blank">
+              <TwitterIcon />
+              Twitter
+            </SocialLink>
+            <SocialLink href="https://github.com/pavelkeyzik" target="_blank">
+              <GitHubIcon />
+              GitHub
+            </SocialLink>
+          </Nav>
+        </FooterContent>
       </ContentWidth>
     </Root>
   );
@@ -20,6 +33,32 @@ const Root = styled.footer`
   align-items: center;
   padding: 20px 0;
   color: ${(props) => props.theme.color.footerTextForeground};
+`;
+
+const FooterContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media screen and (min-width: 600px) {
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+`;
+
+const Nav = styled.nav`
+  & > * {
+    margin-left: 24px;
+  }
+`;
+
+const SocialLink = styled.a`
+  display: inline-flex;
+  align-items: center;
+
+  & > * {
+    margin-right: 8px;
+  }
 `;
 
 export { Footer };
